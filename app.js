18472b87cf488af7b3d5fa9b0ee9ed7107a29403
Lucstay11 <tues.banni@gmail.com>
2023-08-02 23:37:56 +0200

    s

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-onst express = require("express");
+const express = require("express");
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
